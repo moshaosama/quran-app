4e079c008efe6566575adfd02c4f36c0baf79312
moshaosama <mohamedOSFekry@gmail.com>
2023-09-12 02:13:32 +0300

    created a new file

--- src/components/Quran.js
+++ src/components/Quran.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchSurah } from "../Store/Reducers/SurahSlice";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import ReactAudioPlayer from 'react-audio-player';
+import { Link } from "react-router-dom";
 
 function Quran () {
     const state = useSelector(state => state.Surah);
@@ -22,9 +23,11 @@ function Quran () {
                                 <Card.Body>
                                     <Card.Title className="d-flex" id="CardTitle">
                                         <h1>{el?.name_arabic}</h1>
-                                        <div className="btn btn-outline-dark" id="Btn">
-                                            Show
-                                        </div>
+                                        <Link to={"Show"}>
+                                            <div className="btn btn-outline-dark" id="Btn">
+                                                Show
+                                            </div>
+                                        </Link>
                                     </Card.Title>
                                     <Card.Text>
                                         <ReactAudioPlayer
---
+++ src/components/Router.js
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Quran from "./Quran";
+import Show from "./Show";
+
+
+function Router_Pages () {
+    return(
+            <Routes>
+                <Route path="/" element={<Quran/>}></Route>
+                <Route path="/Show" element={<Show/>}></Route>
+            </Routes>
+    )
+}
+
+export default Router_Pages;
